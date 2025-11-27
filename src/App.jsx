41b5d27ddcde5999fc56e0ce06925112fd270eeb
engineersohag <engineer.sohag.cse@gmail.com>
2025-11-28 00:23:10 +0600

    New Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import { useState } from "react";
-import { ThemeProvider } from "./context/ThemeContext";
-import Sidebar from "./components/Sidebar";
-import Navbar from "./components/Navbar";
-import Dashboard from "./pages/Dashboard";
+import Welcome from "./components/Welcome";
 
 export default function App() {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
 
   return (
-    <ThemeProvider>
-      <Sidebar isOpen={sidebarOpen} toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
-
-      <div className={`ml-${sidebarOpen ? "64" : "16"} transition-all`}>
-        <Navbar />
-        <Dashboard />
-      </div>
-    </ThemeProvider>
+    <>
+    <h1>sfh</h1>
+      <Welcome></Welcome>
+    </>
   );
 }
