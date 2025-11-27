53e07ab2ebe0e8117f9adf9b899eb30c3d4e4d20
engineersohag <engineer.sohag.cse@gmail.com>
2025-11-28 02:27:52 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -31,9 +31,9 @@ function App() {
         {/* Content Area */}
         <main className="p-6 flex-1 overflow-y-auto space-y-6">
           {/* Cards Grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <WelcomeCard />
-            <Statistics />
+            <Statistics className="md:col-span-2" />
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
            
